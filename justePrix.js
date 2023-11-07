f709d22f416bcc9c57073402c47297fc5062d7d6
Calmes <christophe.calmes@laplateforme.io>
2023-11-07 16:48:11 +0100

    first blood

--- justePrix.js
+++ justePrix.js
@@ -1,22 +1,3 @@
-
-/*const nameInput = document.getElementById("nameOfPlayer");
-const nameOfPlayer = nameInput.value;
-const riskInput = document.getElementById("nameOfPlayer");
-const risk = nameInput.value;*/
-const initialForm = document.getElementById("initialForm");
-initialForm.addEventListener("submit", onFormSubmit);
-function onFormSubmit(event){
-    // Stop le reset de la page.
-    event.preventDefault();
-    const data = new FormData(event.target);
-    const dataObject = Object.fromEntries(data.entries());
-    console.log(dataObject);
-}
-
-//console.log(nameOfPlayer);
-
-/*const message = document.getElementById("message");
-console.log(message);
 function randomPrice(range) {
     return Math.ceil(Math.random()*range);
 }
@@ -26,37 +7,52 @@ function testNumber(data) {
     } else {
         return true
     }
-}*/
-let count = [];
-/*
-//const nameOfPlayer = prompt("Quel est ton petit prenom ?");
-//let risk = parseInt(prompt("Vous aimez prendre des risques donner votre intervale?"))
-if(!testNumber(risk)){
-    alert("Risk n'est pas un nombre.")
-    risk = parseInt(prompt("Vous aimez prendre des risques donner votre intervale?"))
 }
-const price =  randomPrice(risk);
-let win =  false;
-let number = '';
-console.log(price);
-do {
-    number = parseInt(prompt(`Un nombre entre 1 et ${risk} ?`));
-    if(!testNumber(number)){
-        alert("N'est pas un nombre.");
-        number = parseInt(prompt(`Un nombre entre 1 et ${risk} ?`));
-    }
-    count.push(1);
-    if(price === number){
-        win = true;
-    } else if (price < number) {
-        console.log("C'est moins !");
-        alert("C'est moins !");
-    } else {
-        console.log("C'est plus !");
-        alert("C'est plus !");
-    }
 
-} while(!win)
-message.innerText = `Bravo tu as gagné ${price - ((count.length)*(risk*0.1))} € ${nameOfPlayer} en ${count.length} count`;
-console.log(`Bravo tu as gagné ${price - ((count.length)*(risk*0.1))} € ${nameOfPlayer} en ${count.length} count`);
-*/
+const initialForm = document.getElementById("initialForm");
+initialForm.addEventListener("submit", onFormSubmit);
+
+
+function onFormSubmit(event){
+    let count = [];
+    let risk = 0;
+    let win = false;
+    let go = false;
+    // Stop le reset de la page.
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const dataObject = Object.fromEntries(data.entries());
+    console.log(dataObject);
+        nameOfPlayer = dataObject.nameOfPlayer;
+        risk = dataObject.risk;
+        if(risk >0) {go = true;}
+        let price = randomPrice(risk)
+        console.log(price);
+        if (go) {
+            do {
+                number = parseInt(prompt(`Un nombre entre 1 et ${risk} ?`));
+                if(!testNumber(number)){
+                    alert("N'est pas un nombre.");
+                    number = parseInt(prompt(`Un nombre entre 1 et ${risk} ?`));
+                }
+                count.push(1);
+                if(price === number){
+                    win = true;
+                    go = false;
+                } else if (price < number) {
+                    console.log("C'est moins !");
+                    alert("C'est moins !");
+                } else {
+                    console.log("C'est plus !");
+                    alert("C'est plus !");
+                }
+            
+            } while(!win)
+        }
+  
+        message.innerText = `Bravo tu as gagné ${price - ((count.length)*(risk*0.1))} € ${nameOfPlayer} en ${count.length} count`;
+        console.log(`Bravo tu as gagné ${price - ((count.length)*(risk*0.1))} € ${nameOfPlayer} en ${count.length} count`);
+    
+
+    
+}
